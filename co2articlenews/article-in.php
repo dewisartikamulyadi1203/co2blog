341390fbace448da148a84542229d35f7274665f
rasyahroel <72621641+rasyahroel@users.noreply.github.com>
2021-09-28 12:12:12 +0700

    Frontend from Rasya

---
+++ co2articlenews/article-in.php
@@ -0,0 +1,61 @@
+<?php
+	include 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="article-in">
+		<h1>Dampak Buruk Mie Instan</h1>
+		<div class="article-in-left">
+			<img src="img/mie.jpg" alt="Mie Goreng Korea">
+			<p>Mie instan adalah makanan yang rasannya sangat enak. Tak heran bila makanan in banyak dicintai oleh masyarakat. Namun ternyata sering mengkonsumsi mie instan membawa dampak buruk bagi tubuh. Berdasarkan....</p>
+		</div>
+		<div class="article-in-right">
+			<h4>Popular <span class="text-yellow">Articles</span></h4>
+			<div class="article-in-right-item">
+				<div class="article-in-right-popular">
+					<div class="article-in-right-popular-detail">
+						<h6>Dampak Buruk Mie Instan</h6>
+						<p>Mie instan adalah makanan yang rasanya sangat enak...</p>
+					</div>
+					<div class="article-in-right-popular-img">
+						<img src="img/mie.jpg" alt="Mie Goreng Korea">
+					</div>
+				</div>
+
+				<div class="article-in-right-popular">
+					<div class="article-in-right-popular-detail">
+						<h6>Dampak Buruk Mie Instan</h6>
+						<p>Mie instan adalah makanan yang rasanya sangat enak...</p>
+					</div>
+					<div class="article-in-right-popular-img">
+						<img src="img/mie.jpg" alt="Mie Goreng Korea">
+					</div>
+				</div>
+
+				<div class="article-in-right-popular">
+					<div class="article-in-right-popular-detail">
+						<h6>Dampak Buruk Mie Instan</h6>
+						<p>Mie instan adalah makanan yang rasanya sangat enak...</p>
+					</div>
+					<div class="article-in-right-popular-img">
+						<img src="img/mie.jpg" alt="Mie Goreng Korea">
+					</div>
+				</div>
+			</div>
+			<h5>See More ...</h5>
+				
+
+			
+		</div>
+	</div>
+	
+</body>
+</html>
+<?php
+	include 'footer.php';
+?>
---
+++ co2articlenews/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="header">
+		<img src="img/logo.png" class="logo">
+		<div class="header-right">
+		    <a class="active" href="#home">PROFILES</a>
+		    <a href="#contact">NEWS</a>
+		    <a href="#about">ARTICLES</a>
+		    <a href="#about">COMMENTS</a>
+		</div>
+	</div>
+</body>
+</html>
